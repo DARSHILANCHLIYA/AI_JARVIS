bdfe37a41d4aaf09e52fc39cd86132c017afea3f
DARSHILANCHLIYA <darshilanchliya36@gmail.com>
2024-02-11 17:17:01 +0530

    print how much time taken to execute task And deleting Coversations

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from func.Speak import Speak
 from llm.GPT4 import ChatGpt
 from llm.filter import Filter
 
-import datetime
+import datetime , time
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
@@ -17,15 +17,48 @@ def wishMe():
     else:
         Speak("Good Evening!","I am Friday Sir. Please tell me how may I help you")  
 
+wishMe()
+
+Previous_Chat = open(r"D:\Jarvis\JARVIS\data\chat.txt", "r")
+Previous_Chat = Previous_Chat.read()
+
+ChatGpt(f"Our prevoius chat Chat now according to this{Previous_Chat}. NOTE don't reply only for this command. I gave you this command to just rember past chats")
+
+time.sleep(3)
 
 while 1:
     Query = Listen().lower()
 
-    if 'gpt' in Query:
+    if 'delet' in Query and 'chat' in Query:
+
+        Speak("Deleting all memory sir")
+        
+        file_delet = open("D:\Jarvis\JARVIS\data\chat.txt", "w")
+        file_delet.write("")
+        file_delet.close() 
+
+        Speak("Sir now All conversation of us is deleted")
+
+    elif 'gpt' in Query:
+
+        Query = Query.replace("gpt",'')
+        begin = time.time()
+
         response = ChatGpt(f"REPLY IN LESS WORDS {Query}")
+        end = time.time()
+
+        print(end - begin)
         Speak(response)
 
+        f = open("D:\Jarvis\JARVIS\data\chat.txt", "a")
+        f.writelines(f"YOU:  {Query}\n")
+        f.writelines(f" AI:  {response}\n")
+        f.close()        
+
     elif 'jarvis' in Query:
+        
+
+        begin = time.time()
 
         Query = Query.replace("jarvis","")
         code = ChatGpt(f"{Query} ***use python programing language. just write complete code nothing else, also don't dare to use input function*** **you can use the module that i provided if required**")
@@ -33,6 +66,9 @@ while 1:
 
         try:
             exec(code)
-        except Exception as e:
-            Speak("Sorry Sir geeting Some error.")
+            end = time.time()
 
+            print(end - begin)
+        except Exception as e:
+            Speak("Sorry Sir getting Some error.")
+            
